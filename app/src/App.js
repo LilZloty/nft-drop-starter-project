02a7843811e1082b90fb8bd38272848bcacc4fa9
Théo Daudebourg <54123008+LilZloty@users.noreply.github.com>
2021-12-08 12:38:22 -0600

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -15,10 +15,20 @@ const App = () => {
   const checkIfWalletIsConnected = async () => {
     try {
       const { solana } = window;
-
+  
       if (solana) {
         if (solana.isPhantom) {
           console.log('Phantom wallet found!');
+  
+          /*
+           * The solana object gives us a function that will allow us to connect
+           * directly with the user's wallet!
+           */
+          const response = await solana.connect({ onlyIfTrusted: true });
+          console.log(
+            'Connected with Public Key:',
+            response.publicKey.toString()
+          );
         }
       } else {
         alert('Solana object not found! Get a Phantom Wallet 👻');
@@ -27,7 +37,6 @@ const App = () => {
       console.error(error);
     }
   };
-
   /*
    * When our component first mounts, let's check to see if we have a connected
    * Phantom Wallet
